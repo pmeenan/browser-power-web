56ea55ab313f41cf848b6dd6925b6c8d1bb1a1c2
Patrick Meenan <pmeenan@chromium.org>
2015-04-21 14:28:55 -0400

    Added logging as tasks are processed

--- getTask.php
+++ getTask.php
@@ -4,12 +4,19 @@ if (isset($_REQUEST['test'])) {
   $id = $_REQUEST['test'];
   $test = TestGetState($id);
   if ($test !== false) {
+    // see if we have anything to log from the last task
+    if (isset($_REQUEST['log'])) {
+      $data = json_decode($_REQUEST['log'], true);
+      if (isset($data) && is_array($data))
+        TestLog($id, TEST_DATA, $data);
+    }
     $task = TestGetNextTask($test);
   }
 }
 
 if (isset($task) && $task !== false) {
   TestSaveState($id, $test);
+  TestLog($id, TEST_ACTION, $task);
   $ret = array('result' => 200, 'task' => $task);
   json_response($ret);
 } else {
