907de482dfd8021d88bdc7841887f0574e27956a
Patrick Meenan <patmeenan@gmail.com>
2015-04-30 18:43:20 -0400

    Switched the URL list back to the default

--- startTest.php
+++ startTest.php
@@ -9,7 +9,7 @@ $test = array('UA' => $_SERVER['HTTP_USER_AGENT'],
 if (isset($_REQUEST['label']))
   $test['label'] = $_REQUEST['label'];
 //$test['list'] = 'no video';
-$test['list'] = 'h264';
+$test['list'] = 'default';
 if (isset($_REQUEST['list']) && preg_match('/^[a-zA_Z0-9\- ]+$/', $_REQUEST['list']))
   $test['list'] = $_REQUEST['list'];
 $id = TestCreate($test);
